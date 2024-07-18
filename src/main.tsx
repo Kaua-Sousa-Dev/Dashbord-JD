94f6e50a41a6baff970c44e1c5899b6ac5a322f5
não troca zé <oi!231@gmail.com>
2024-07-18 16:41:36 -0300

    Alterate

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 
-import { createBrowserRouter, RouterProvider, RouterProviderProps } from 'react-router-dom'
 
 import ErrorPage from './routers/ErrorPage'
 import Home from './routers/Home'
