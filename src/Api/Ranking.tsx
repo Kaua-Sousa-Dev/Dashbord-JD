b2abb6bc507e60499cb23260447030f09b12bae0
Kaua <sousakaua7@gmail.com>
2024-08-01 16:58:13 -0300

    Ranking function completed

--- src/Api/Ranking.tsx
+++ src/Api/Ranking.tsx
@@ -1,11 +1,12 @@
+// importando dados
 import { useState } from "react";
 import * as XLSX from "xlsx";
 import { Iaddinf } from "../interfaces/columnsXML";
 import exemplo_De_Planilha from "/Exemplo_De_Planilha-png.png";
 import ReactPaginate from "react-paginate";
-import "../css/FormLabel.css"
+import "../css/FormLabel.css";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
+import "react-toastify/dist/ReactToastify.css";
 
 // Importando components
 import Container from "../components/Body/Container";
@@ -21,27 +22,46 @@ import Ex_pl from "../components/image/Ex_Pl";
 import Td from "../components/Form/Td";
 
 function Ranking() {
-  // Trabalhando com dados
+  // dados de conversão
   const [data, setData] = useState<Iaddinf[]>([]);
   const [filteredData, setFilteredData] = useState<Iaddinf[]>([]);
   const [usersPerPage] = useState(10);
   const [pageNumberOriginal, setPageNumberOriginal] = useState(0);
   const [course, setCourse] = useState<string>("");
 
-  // Definindo Tamanho do arquivo
+  // tamanho arquivo xls
   const MAX_FILE_SIZE = 512 * 1024;
 
-  // Constante de Leitura do arquivo e validações
+  // Mapeamento de palavras-chave para interesses
+  const keywordMapping = [
+    { interest: "programação", keywords: ["java", "python", "c++", "php", "desenvolvedor", "dev", "desenvolvimento", "web", "mysql", "banco de dados", "frontend", "backend", "fullstack", "python", "android", "ios", "gestão de projetos", "mobile", "ruby", "swift", "c#", "go", "kotlin", "r", "sql", "html", "css", "typescript", "rust", "framework", "nosql", "algoritmo"] },
+
+    { interest: "marketing", keywords: ["estratégia digital", "social media", "copywriter", "anúncios", "marca", "análise de mercado", "campanhas", "redes sociais", "publicidade", "branding", "publicidade", "propaganda", "publicidade e propaganda"] },
+
+    { interest: "design", keywords: ["ui", "ux", "photoshop", "gráfico", "design think", "design gráfico", "interface", "experiência de usuário", "interface de usuário", "visual", "logo", "layout", "figma", "after effects", "animação", "motion graphics", "arte digital", "arte", "modelagem", "renderização", "visualização 3D", "graphic design", "design gráfico", "web design", "ilustration", "3D design", "interior design", "packaging design", "print design", "adverting design", "visual design"] },
+
+    { interest: "hardware", keywords: ["manutenção de computadores", "manutenção", "computadores", "montagem", "pc", "pcs", "cabeamento", "configuração de redes", "roteadores", "switches", "redes", "lan", "wan", "protocolos de rede", "eletrônica", "periféricos", "configuração", "dispositivos de entrada/saida", "armazenamento de dados", "hd", "ssd", "sistemas embarcados", "suporte", "suporte técnico", "diagnóstico de hardware", "fontes de alimentação", "impressoras"]},
+
+    { interest: "dados", keywords: ["power bi", "ferramentas google", "análise de dados", "estátisticas", "visualização de dados", "dashboards", "big data", "data", "banco de dados", "sql", "nosql", "modelagem de dados", "banco de dados relacionais", "gerenciamento de dados", "data science", "ciência de dados", "armazenamento de dados", "estruturas de dados", "backup e recuperação", "engenharia de dados", "segurança de dados", "data mining", "processamento de dados", "data analytics", "analytics"]},
+    
+    { interest: "inteligência artificial", keywords: ["machine learn", "ia", "ai", "data science", "redes neurais", "reconhecimento de fala", "reconhecimento de movimento", "processamento de linguagem natural", "visão computacional", "automação"]},
+
+    { interest: "games", keywords: ["design de jogos", "programação de jogos", "engine de jogos", "unity", "unreal engine", "mecânicas de jogo", "prototipagem de jogos", "balanceamento de jogo", "jogo", "jogos", "scripts para jogos", "otimização de desempenho", "desenvolvimento de jogos", "2D", "3D", "animação de personagens", "personagens", "texturização", "efeitos visuais", "animação facial", "modelagem", "animação", "áudio em jogos", "efeitos sonoros", "música para jogos", "criação de ambientes sonoros", "experiência do usuário em jogos", "desenvolvimento multijogador", "multijogador", "jogos online", "testes e qualidade", "teste de jogos", "mercado de jogos"]},
+
+    { interest: "robótica", keywords: ["controle de robôs", "robôs", "interação humano-robô", "cinemática", "dinâmica", "sensores e atuadores", "reconhecimento de objetos", "redes e sensores", "robôs domésticos", "simulação de robôs"]}
+  ];
+
+  // leitura de arquivo e renderização
   const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
-      // Validação
+      // verificação de tamanho
       if (file.size > MAX_FILE_SIZE) {
         toast.error("O arquivo é muito grande. O tamanho máximo permitido é 512KB.");
         return;
       }
 
-      // Lendo arquivo
+      // verificação de arquivo
       const FileType = file.name.split(".").pop()?.toLowerCase();
       if (FileType === "xls" || FileType === "xlsx") {
         const reader = new FileReader();
@@ -58,12 +78,12 @@ function Ranking() {
             const mappedData = mapData(data);
             setData(mappedData);
             setFilteredData(mappedData);
-            toast.success("Arquivo carregado com sucesso!")
+            toast.success("Arquivo carregado com sucesso!");
           }
         };
         reader.readAsArrayBuffer(file);
       } else {
-        toast.error("Selecione um arquivo .xls ou .xlsx!")
+        toast.error("Selecione um arquivo .xls ou .xlsx!");
       }
     }
   };
@@ -78,38 +98,46 @@ function Ranking() {
     });
   };
 
-  const interestCategoryMap : {[key: string] : string[]} = {
-    "Programação": ["Desenvolvimento", "Web"]
-  }
+  // filtrando interesses
+  const findInterest = (keyword: string) => {
+    const entry = keywordMapping.find((entry) =>
+      entry.keywords.some(k => k.toLowerCase() === keyword.toLowerCase())
+    );
+    return entry ? entry.interest : null;
+  };
 
-  const rankedStudents = (course: string) =>{
-    let relatedInterest: string[] = [];
-    for(const [interest, course  ] of Object.entries(interestCategoryMap)) {
-      if(course.map(c = c.toLowerCase()).includes(course.toLowerCase())) {
-        relatedInterest = interestCategoryMap[interest]
-        break
-      }
-    }
-  }
-
-  // Algoritmo de Ranking
+  // rankeando estudantes
   const rankStudents = (course: string) => {
+    const interest = findInterest(course.toLowerCase());
+
+    if (!interest) {
+      toast.error("Nenhuma informação encontrada sobre os dados inseridos!");
+      setFilteredData(data);
+      return;
+    }
+
     const relatedStudents = data.filter(student =>
-      student.Interesse.toLowerCase().includes(course.toLowerCase())
+      student.Interesse.toLowerCase().includes(interest.toLowerCase())
     );
     const unrelatedStudents = data.filter(student =>
-      !student.Interesse.toLowerCase().includes(course.toLowerCase())
+      !student.Interesse.toLowerCase().includes(interest.toLowerCase())
     );
 
     setFilteredData([...relatedStudents, ...unrelatedStudents]);
   };
 
-  // React-Paginate (Primeira Lista)
+  // acionar Enter
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      rankStudents(course);
+    }
+  };
+
+  // react-paginate
   const handlePageClickOriginal = ({ selected }: { selected: number }) => {
     setPageNumberOriginal(selected);
   };
 
-  // Constantes React-Paginate Config
   const pagesVisitedOriginal = pageNumberOriginal * usersPerPage;
 
   const currentDataOriginal = filteredData.slice(
@@ -128,7 +156,6 @@ function Ranking() {
           ao uso de caracteres especiais
         </P>
         
-        {/* Input de seleção de arquivo */}
         <form className="ButtonInput">
           <label htmlFor="arquivo">
             <span>Selecione um arquivo</span>
@@ -145,7 +172,6 @@ function Ranking() {
         <Ex_pl src={exemplo_De_Planilha} alt="Exemplo de Planilha" />
       </Container>
 
-      {/* Caso exista um arquivo compatível, após a leitura obtendo valores maiores que 0 irá ser adicionado a lista dos dados e opções de filtragens */}
       {data.length > 0 && (
         <Container>
           <Title>Dados da Planilha:</Title>
@@ -154,9 +180,10 @@ function Ranking() {
             placeholder="Digite o curso (ex: Java)" 
             value={course} 
             onChange={(e) => setCourse(e.target.value)} 
+            onKeyDown={handleKeyDown}
           />
           <button onClick={() => rankStudents(course)}>Rankear Alunos</button>
-          <ContainerTable> {/* Primeira tabela de dados lidos */}
+          <ContainerTable>
             <ListPrinc>
               <Thead>
                 <Tr>
@@ -217,7 +244,6 @@ function Ranking() {
               </Tbody>
             </ListPrinc>
           </ContainerTable>
-          {/* React-Paginate Config */}
           <ReactPaginate
             previousLabel={"Anterior"}
             nextLabel={"Próximo"}
